cb2abbd6acb0125998a5d858dcd3f2d473881716
Sylwia Werner <werner.sylwia38@gmail.com>
2026-06-01 07:05:20 +0200

    chore: setup project foundation

---
+++ src/shared/components/layout/container.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@/lib/utils";
+import type { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  className?: string;
+};
+
+export function Container({ children, className }: Props) {
+  return (
+    <div className={cn("container mx-auto px-4", className)}>{children}</div>
+  );
+}
