c337765067e675987cab8743427dc13c45c4b8c6
Sylwia Werner <werner.sylwia38@gmail.com>
2026-06-01 09:06:08 +0200

    chore: allow dummyjson remote images

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+    images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
